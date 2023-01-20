7253211857198399d1fc4ef6e6e4f73f5fe939f4
kjaindevops <k.jain.devops@gmail.com>
2023-01-20 22:06:23 +0530

    Intoduction to variables and tfvars

---
+++ Terraform_Codes/introduction_to_variables.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  backend "s3" {
+    key      = "terraform.tfstate"
+    bucket   = "kunaldevopsbucket"
+    region   = "eu-west-1"
+    dynamodb = "kunal.tf"
+  }
+}
+
+# In this block we have given the variable refference to the cidr_block output and asked terraform to look for the vailables in different file.
+resource "aws_vpc" "Kunal_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = "Kunal_vpc"
+    Environment = "Dev"
+  }
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.Kunal_vpc
+}
---
+++ Terraform_Codes/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr" {
+  description = "Choose the cidr_block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
