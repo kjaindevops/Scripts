1b0e766117cbc11916c3115272b6152b0bc3413e
kjaindevops <k.jain.devops@gmail.com>
2023-01-30 23:36:40 +0530

    Made correction to private subnets block

--- Terraform_Codes/VPC_Creation_from_Scratch/private_subnets.tf
+++ Terraform_Codes/VPC_Creation_from_Scratch/private_subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "KunalPrivateSubnets" {
-  count             = length(slice(local.azs_names))
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
+  count             = length(slice(local.azs_names, 0, 2))
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + length(local.azs_names))
   vpc_id            = aws_vpc.Kunal_VPC.id
   availability_zone = local.azs_names[count.index]
 
